0808c3e310daff937987b458269a2c13b663e6ed
JonathanAlexNg <jonathanalexng@outlook.com>
2021-01-21 14:39:57 +0800

    pylint done

--- lint_static.py
+++ lint_static.py
@@ -1,6 +1,32 @@
-
 # For passing linting and static
 
-'''Describe code'''
+'''From number 1-20, pick a GUESS that the prog has
+randomly selected, if correct win $5, wrong lose $1.
+Can try as many times until want to quit.'''
 
+import random
+import sys
 
+START = input('I will randomly pick a number between 1-6\n\
+Guess it correctly & gain $5. Wrong, lose $1.\n\
+Ready to play? (y/n) ')
+
+if START == 'n':
+    sys.exit()
+else:
+
+    BALANCE = 0
+
+    while True:
+        CHOSEN = random.randint(1, 6)
+        GUESS = input('\nYour GUESS (q to quit): ')
+        if GUESS == 'q':
+            sys.exit()
+        else:
+            pass
+        if int(GUESS) == CHOSEN:
+            BALANCE += 5
+        else:
+            BALANCE -= 1
+        print('Guessed: {} ANSWER: {}'.format(GUESS, CHOSEN))
+        print('Current  BALANCE: $', BALANCE)
